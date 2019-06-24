db3e69aaaa11917e040eb407f290b028b9bf67b7
zhuguoliang <zhugl@taofen8.com>
2019-06-24 17:17:38 +0800

    增加一些配置

---
+++ build/precache-manifest.7def60519a584594b5db66c3ba6233e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28fa5ac59985f0abbdc2de7916af5911",
+    "url": "/index.html"
+  },
+  {
+    "revision": "026a7fdead9552bb5f00",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "026a7fdead9552bb5f00",
+    "url": "/static/js/main.958fe95b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -2,23 +2,36 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>标准项目模版</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+class App extends React.Component {
+  handleClick(num) {
+    let k = 9
+    var nums = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    if (k > nums.length) {
+      k = k % nums.length
+    }
+    let m = nums.splice(nums.length - k)
+    nums = m.concat(nums)
+    console.log(nums)
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handleClick}>点我</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App
