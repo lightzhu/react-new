858e02d05b29d6fdd363a5d8d29fa481cf23fc18
zhuguoliang <zhugl@taofen8.com>
2019-07-16 18:30:04 +0800

    增加新闻类型的选择

--- src/pages/news/article.js
+++ src/pages/news/article.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import { List } from 'antd'
+import { List, Avatar } from 'antd'
 // import '../../style/weather.scss'
 // import store from '../../reducers/store'
 // import { getWeatherInfo } from '../../actions/Index'
 const Aticle = list => {
   return (
-    <div
-      onClick={() => {
-        list.onClick('top')
-      }}
-    >
+    <div>
       <List
         dataSource={list.content.data}
         renderItem={item => (
           <List.Item>
-            <h4>{item.title}</h4>
+            <List.Item.Meta
+              avatar={<Avatar src={item.thumbnail_pic_s} />}
+              title={<a href={item.url}>{item.title}</a>}
+              description={item.date + item.author_name}
+            />
           </List.Item>
         )}
       />
--- src/pages/news/news.js
+++ src/pages/news/news.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Affix } from 'antd'
-import '../../style/weather.scss'
+import '../../style/home.scss'
 import { connect } from 'react-redux'
 import { getNewsInfo } from '../../actions/Index'
 import Aticle from './article'
@@ -10,7 +10,49 @@ class News extends React.Component {
     this.state = {
       data: [],
       loading: true,
-      type: 'plus'
+      type: 'top',
+      newsType: [
+        {
+          name: '头条',
+          val: 'top'
+        },
+        {
+          name: '社会',
+          val: 'shehui'
+        },
+        {
+          name: '国内',
+          val: 'guonei'
+        },
+        {
+          name: '国际',
+          val: 'guoji'
+        },
+        {
+          name: '娱乐',
+          val: 'yule'
+        },
+        {
+          name: '体育',
+          val: 'tiyu'
+        },
+        {
+          name: '军事',
+          val: 'junshi'
+        },
+        {
+          name: '科技',
+          val: 'keji'
+        },
+        {
+          name: '财经',
+          val: 'caijing'
+        },
+        {
+          name: '时尚',
+          val: 'shishang'
+        }
+      ]
     }
   }
   render() {
@@ -20,18 +62,41 @@ class News extends React.Component {
         <Affix>
           <h2>今日要闻</h2>
         </Affix>
+        <select onChange={this.newTypeChange.bind(this)}>
+          {this.renderSelect(this.state.newsType)}
+        </select>
         <div className="news-box">
-          <Aticle
-            content={this.props.data}
-            title="fdjdsfjkh"
-            onClick={this.props.handleClick.bind(this)}
-          />
+          <Aticle content={this.props.data} title="fdjdsfjkh" />
         </div>
       </div>
     )
   }
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.getInitData(this.state.type)
+  }
   componentWillUpdate() {}
+  renderSelect(types) {
+    let arr = []
+    types.forEach((element, index) => {
+      arr.push(
+        <option key={index} value={element.val}>
+          {element.name}
+        </option>
+      )
+    })
+    return arr
+  }
+  newTypeChange(e) {
+    console.log(e.target.value)
+    this.setState(
+      {
+        type: e.target.value
+      },
+      () => {
+        this.props.getInitData(this.state.type)
+      }
+    )
+  }
 }
 const mapStateToProps = state => {
   return {
@@ -41,7 +106,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    handleClick: msg => {
+    getInitData: msg => {
       dispatch(getNewsInfo(msg))
     }
   }
