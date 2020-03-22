426ff449da4505cd67462a79564dab4601ba4d1c
zhuguoliang <zhugl@taofen8.com>
2020-03-22 21:36:08 +0800

    开始树洞模块

---
+++ src/components/EchoList.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { ListView, Icon } from 'antd-mobile';
+import heart from '../static/icon/heart.png'
+import message from '../static/icon/message.png'
+import './index.scss'
+class EchoList extends Component {
+  constructor(props) {
+    super(props);
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2,
+    });
+    this.state = {
+      dataSource,
+      height: document.documentElement.clientHeight - 40,
+    };
+  }
+
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.rData = genData();
+  //     this.setState({
+  //       dataSource: this.state.dataSource.cloneWithRows(this.rData),
+  //       isLoading: false,
+  //     });
+  //   }, 600);
+  // }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.data !== this.props.data) {
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(nextProps.data),
+      });
+    }
+  }
+
+  onEndReached = (event) => {
+    console.log('reach end', event);
+    this.props.loadMore();
+    // this.setState({ isLoading: true });
+    // setTimeout(() => {
+    //   this.rData = { ...this.rData, ...genData(++pageIndex) };
+    //   this.setState({
+    //     dataSource: this.state.dataSource.cloneWithRows(this.rData),
+    //     isLoading: false,
+    //   });
+    // }, 1000);
+  }
+
+  render() {
+    const { data, loading } = this.props
+    let index = data.length - 1;
+    const row = (rowData, sectionID, rowID) => {
+      if (index < 0) {
+        index = data.length - 1;
+      }
+      const obj = data[index--];
+      return (
+        <div key={rowID} style={{ padding: '0 15px', borderTop: '20px solid #f5f5f9' }}>
+          <div className="content-head">
+            <h5>
+              <img src="https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg"></img>
+              <span>用户名</span>
+            </h5>
+          </div>
+          <div className="content-body">
+            <p>This is content of `Card`,This is content of `Card`,This is content of `Card`
+            This is content of `Card`,This is content of `Card`
+            </p>
+            <p className="img-box">
+              <img src='//wx3.sinaimg.cn/mw600/0085KTY1gy1gd1xptnkdoj31400u0e81.jpg' alt="" />
+            </p>
+          </div>
+          <div className="content-foot"><img src={heart} alt='' /><img src={message} alt='' /></div>
+        </div>
+      );
+    };
+    return (
+      <ListView
+        ref={el => this.lv = el}
+        dataSource={this.state.dataSource}
+        renderFooter={() => (<div style={{ padding: 10, textAlign: 'center' }}>
+          {loading ? 'Loading...' : 'Loaded'}
+        </div>)}
+        renderRow={row}
+        className="am-list"
+        pageSize={4}
+        style={{
+          height: this.state.height,
+          overflow: 'auto',
+        }}
+        useBodyScroll={false}
+        scrollRenderAheadDistance={500}
+        onEndReached={this.onEndReached}
+        onEndReachedThreshold={10}
+      />
+    );
+  }
+}
+export default EchoList;
--- src/pages/echo/index.js
+++ src/pages/echo/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { NoticeBar, PullToRefresh, Icon } from 'antd-mobile'
+import { NoticeBar, PullToRefresh, Icon, Toast, ListView } from 'antd-mobile'
+import EchoList from '../../components/EchoList'
 function genData() {
   const dataArr = [];
   for (let i = 0; i < 20; i++) {
@@ -10,11 +11,14 @@ function genData() {
 class Echo extends Component {
   constructor(props) {
     super(props)
+
     this.state = {
       refreshing: false,
       down: true,
       height: document.documentElement.clientHeight - 80,
-      data: []
+      data: [],
+      loading: true,
+      hasMore: true
     }
   }
   componentDidMount() {
@@ -23,7 +27,14 @@ class Echo extends Component {
       data: genData(),
     }), 0);
   }
+  loadMore() {
+    if (this.state.hasMore) {
+      // this.getMovieList()
+      console.log('获取更多')
+    }
+  }
   render() {
+    const { data, loading } = this.state
     return (<div>
       <NoticeBar marqueeProps={{ loop: true, style: { padding: '0 7.5px' } }}>
         Notice: 你大爷发布了一条消息，大家都在讨论，快去围观
@@ -45,11 +56,12 @@ class Echo extends Component {
           }, 1000);
         }}
       >
-        {this.state.data.map(i => (
+        {/* {this.state.data.map(i => (
           <div key={i} style={{ textAlign: 'center', padding: 20 }}>
             {this.state.down ? 'pull down' : 'pull up'} {i}
           </div>
-        ))}
+        ))} */}
+        <EchoList data={data} loading={loading} loadMore={this.loadMore}></EchoList>
       </PullToRefresh>
     </div>)
   }
