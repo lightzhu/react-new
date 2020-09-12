da16be9c496113e6bf2d5da8fd44f482d802cf4f
zhuguoliang <zhugl@taofen8.com>
2020-09-12 15:28:36 +0800

    添加编译文件

---
+++ build/precache-manifest.309f324179102832aef59a79363f3f1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36d4ef88fb452752def21440b815163",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23be17baa9b73800cf60",
+    "url": "./static/css/2.3325157f.chunk.css"
+  },
+  {
+    "revision": "bffdd9318defa9202362",
+    "url": "./static/css/main.b2ceedff.chunk.css"
+  },
+  {
+    "revision": "23be17baa9b73800cf60",
+    "url": "./static/js/2.ebf736f2.chunk.js"
+  },
+  {
+    "revision": "bffdd9318defa9202362",
+    "url": "./static/js/main.33d19c94.chunk.js"
+  },
+  {
+    "revision": "30357168f7dcb2ceafa1",
+    "url": "./static/js/runtime~main.0b97ba0c.js"
+  },
+  {
+    "revision": "74e062cc3caa780b828e1c4d2e3e3a59",
+    "url": "./static/media/qingpi.74e062cc.jpg"
+  }
+]);
